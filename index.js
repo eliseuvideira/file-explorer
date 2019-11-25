d8a63180356dfd6c52caa5b0a10df831f6a326f8
Eliseu Videira <eliseuvideira1@gmail.com>
2019-11-24 22:09:53 -0300

    Handling the actions of the titlebar

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 /**@type {BrowserWindow} */
 let browserWindow = null;
 
 app.on('ready', () => {
-  browserWindow = new BrowserWindow({ show: false });
+  browserWindow = new BrowserWindow({
+    show: false,
+    webPreferences: { nodeIntegration: true },
+  });
   browserWindow.loadURL('http://localhost:3000');
   browserWindow.on('ready-to-show', () => {
     browserWindow.show();
   });
+});
+
+ipcMain.on('close', () => {
+  const win = BrowserWindow.getFocusedWindow();
+  win.close();
+});
+
+ipcMain.on('minimize', () => {
+  const win = BrowserWindow.getFocusedWindow();
+  win.minimize();
+});
+
+ipcMain.on('maximize', () => {
+  const win = BrowserWindow.getFocusedWindow();
+  win.maximize();
+});
+
+ipcMain.on('restore', () => {
+  const win = BrowserWindow.getFocusedWindow();
+  win.restore();
 });
--- src/components/Titlebar.js
+++ src/components/Titlebar.js
@@ -1,16 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Titlebar.css';
 
-function Header() {
-  return (
-    <header className="l-app__titlebar titlebar">
-      <span className="titlebar__path">/home/eliseu/nodejs/file-explorer</span>
-      <a className="titlebar__btn"></a>
-      <a className="titlebar__btn is-hidden"></a>
-      <a className="titlebar__btn"></a>
-      <a className="titlebar__btn"></a>
-    </header>
-  );
+const { ipcRenderer } = window.require('electron');
+
+class TitleBar extends Component {
+  state = {
+    isMaximized: false,
+  };
+
+  close = (event) => {
+    event.preventDefault();
+    ipcRenderer.send('close');
+  };
+
+  minimize = (event) => {
+    event.preventDefault();
+    ipcRenderer.send('minimize');
+  };
+
+  maximize = (event) => {
+    event.preventDefault();
+    ipcRenderer.send('maximize');
+    this.setState({ isMaximized: true });
+  };
+
+  restore = (event) => {
+    event.preventDefault();
+    ipcRenderer.send('restore');
+    this.setState({ isMaximized: false });
+  };
+
+  render() {
+    return (
+      <header className="l-app__titlebar titlebar">
+        <span className="titlebar__path">
+          /home/eliseu/nodejs/file-explorer
+        </span>
+        <span className="titlebar__btn" onClick={this.minimize}>
+          _
+        </span>
+        {this.state.isMaximized ? (
+          <span className="titlebar__btn" onClick={this.restore}>
+            ⊠
+          </span>
+        ) : (
+          <span className="titlebar__btn" onClick={this.maximize}>
+            ◻
+          </span>
+        )}
+        <span className="titlebar__btn" onClick={this.close}>
+          ×
+        </span>
+      </header>
+    );
+  }
 }
 
-export default Header;
+export default TitleBar;
